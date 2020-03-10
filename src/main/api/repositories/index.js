2cd3995cf9b487ca1754ab110795953c3abc5ba3
Michael Braun <michael@web4brauns.de>
2020-03-10 22:23:24 +0100

    Add fetch-endpoint

--- src/main/api/repositories/index.js
+++ src/main/api/repositories/index.js
@@ -1,7 +1,9 @@
 import listRepositories from './list';
 import getRepositoryStatus from './get-status';
+import fetch from './actions/fetch';
 
 export default function createRepositoryEndpoints(server) {
     server.get('/repositories', listRepositories);
     server.get('/repositories/:repositoryId/status', getRepositoryStatus);
+    server.post('/repositories/:repositoryId/fetch', fetch);
 }
