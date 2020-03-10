8f43adbc9f94caa086d212a663fe13ad22053982
Michael Braun <michael@web4brauns.de>
2020-03-10 22:31:59 +0100

    Add pull-endpoint

--- src/main/api/repositories/index.js
+++ src/main/api/repositories/index.js
@@ -1,9 +1,11 @@
 import listRepositories from './list';
 import getRepositoryStatus from './get-status';
 import fetch from './actions/fetch';
+import pull from './actions/pull';
 
 export default function createRepositoryEndpoints(server) {
     server.get('/repositories', listRepositories);
     server.get('/repositories/:repositoryId/status', getRepositoryStatus);
     server.post('/repositories/:repositoryId/fetch', fetch);
+    server.post('/repositories/:repositoryId/pull', pull);
 }
