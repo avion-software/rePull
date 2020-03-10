f292e02cba9a8b3a23e34c87d52da72d0529d700
Michael Braun <michael@web4brauns.de>
2020-03-10 22:50:57 +0100

    Add commit endpoint

--- src/main/api/repositories/index.js
+++ src/main/api/repositories/index.js
@@ -2,10 +2,12 @@ import listRepositories from './list';
 import getRepositoryStatus from './get-status';
 import fetch from './actions/fetch';
 import pull from './actions/pull';
+import commit from './actions/commit';
 
 export default function createRepositoryEndpoints(server) {
     server.get('/repositories', listRepositories);
     server.get('/repositories/:repositoryId/status', getRepositoryStatus);
     server.post('/repositories/:repositoryId/fetch', fetch);
     server.post('/repositories/:repositoryId/pull', pull);
+    server.post('/repositories/:repositoryId/commit', commit);
 }
