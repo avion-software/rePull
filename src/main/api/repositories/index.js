7ac3288f9ebea481288e7ecaf3fdc43b16e762ed
Michael Braun <michael@web4brauns.de>
2020-03-10 23:01:05 +0100

    Add push endpoint

--- src/main/api/repositories/index.js
+++ src/main/api/repositories/index.js
@@ -3,6 +3,7 @@ import getRepositoryStatus from './get-status';
 import fetch from './actions/fetch';
 import pull from './actions/pull';
 import commit from './actions/commit';
+import push from './actions/push';
 
 export default function createRepositoryEndpoints(server) {
     server.get('/repositories', listRepositories);
@@ -10,4 +11,5 @@ export default function createRepositoryEndpoints(server) {
     server.post('/repositories/:repositoryId/fetch', fetch);
     server.post('/repositories/:repositoryId/pull', pull);
     server.post('/repositories/:repositoryId/commit', commit);
+    server.post('/repositories/:repositoryId/push', push);
 }
