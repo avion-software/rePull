235418ffb2fd608cc20fa03f0bbc5f12e3fc0b1c
Michael Braun <michael@web4brauns.de>
2020-03-17 21:25:41 +0100

    Add repository-status reducer

--- src/renderer/store/reducers/index.js
+++ src/renderer/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import repositories from './repositories/reducer';
+import repositoryStatus from './repository-status/reducer';
 import selections from './selections/reducer';
 
 const rootReducer = combineReducers({
     repositories,
+    repositoryStatus,
     selections,
 });
 
---
+++ src/renderer/store/reducers/repository-status/reducer.js
@@ -0,0 +1,17 @@
+import { produce } from 'immer';
+import { SET_REPOSITORY_STATUS } from './actions';
+
+const initialState = {
+    values: {},
+};
+
+const repositoryStatus = (baseState = initialState, action) => produce(baseState, (draftState) => {
+    switch (action.type) {
+        case SET_REPOSITORY_STATUS:
+            draftState.values[action.payload.id] = action.payload.value;
+        default:
+            break;
+    }
+});
+
+export default repositoryStatus;
