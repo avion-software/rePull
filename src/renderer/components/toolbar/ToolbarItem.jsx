9f4769ab35941aad42c3b79cd1967fc561a2a4cd
Michael Braun <michael@web4brauns.de>
2020-03-17 19:20:55 +0100

    Add onClick prop to ToolbarItem

--- src/renderer/components/toolbar/ToolbarItem.jsx
+++ src/renderer/components/toolbar/ToolbarItem.jsx
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import useToolbarItemStyles from './ToolbarItemStyles';
 
-const ToolbarItem = ({ icon, label }) => {
+const ToolbarItem = ({ icon, label, onClick }) => {
     const classes = useToolbarItemStyles();
 
     return (
-        <div className={classes.root}>
+        <div
+            className={classes.root}
+            onClick={onClick}
+        >
             <div className={classes.icon}>
                 {icon}
             </div>
@@ -19,7 +22,11 @@ const ToolbarItem = ({ icon, label }) => {
 };
 
 ToolbarItem.propTypes = {
+    onClick: PropTypes.func,
+};
 
+ToolbarItem.defaultProps = {
+    onClick: null,
 };
 
 export default ToolbarItem;
