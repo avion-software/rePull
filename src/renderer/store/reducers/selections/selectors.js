ac41fb813acb036299423ceafb7ea3a1805e0211
Michael Braun <michael@web4brauns.de>
2020-03-20 22:29:10 +0100

    Add showActionOverlay to selections reducer

--- src/renderer/store/reducers/selections/selectors.js
+++ src/renderer/store/reducers/selections/selectors.js
@@ -1,2 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const getActiveRepository = (state) => state.selections.activeRepository;
+export const getShowActionOverlay = (state) => state.selections.showActionOverlay;
