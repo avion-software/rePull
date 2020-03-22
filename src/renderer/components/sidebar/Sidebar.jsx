12b310ac5e6835e21562f1b1db212a3a28a922b3
Michael Braun <michaelsoftware1997@gmail.com>
2020-03-22 15:42:33 +0100

    Add BranchTree to Sidebar

--- src/renderer/components/sidebar/Sidebar.jsx
+++ src/renderer/components/sidebar/Sidebar.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import useSidebarStyles from './SidebarStyles';
+import { REPOSITORY_BRANCH_SHAPE } from '../../constants/shapes';
+import BranchTree from './branch-tree/BranchTree';
 
-const Sidebar = props => {
+const Sidebar = ({ branches }) => {
     const classes = useSidebarStyles();
 
     return (
         <div className={classes.root}>
             Hello Sidebar
+            {branches?.local && (
+                <BranchTree
+                    branches={branches.local}
+                />
+            )}
         </div>
     );
 };
 
 Sidebar.propTypes = {
+    branches: PropTypes.shape({
+        local: PropTypes.arrayOf(PropTypes.shape(REPOSITORY_BRANCH_SHAPE)).isRequired,
+        remotes: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.shape(REPOSITORY_BRANCH_SHAPE))),
+    }),
+};
 
+Sidebar.defaultProps = {
+    branches: null,
 };
 
 export default Sidebar;
