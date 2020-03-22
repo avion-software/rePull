037706ba61ac2b643c0bedc5b19af46eb29c5f14
Michael Braun <michaelsoftware1997@gmail.com>
2020-03-22 14:33:14 +0100

    Add new shape for branches

--- src/renderer/constants/shapes.js
+++ src/renderer/constants/shapes.js
@@ -10,3 +10,17 @@ export const REPOSITORY_STATUS_SHAPE = {
     behind: PropTypes.number.isRequired,
     ahead: PropTypes.number.isRequired,
 };
+
+export const TREE_ITEM_SHAPE = {
+};
+
+TREE_ITEM_SHAPE.children = PropTypes.arrayOf(PropTypes.oneOfType([
+    PropTypes.shape(TREE_ITEM_SHAPE),
+    PropTypes.object,
+])).isRequired;
+
+export const REPOSITORY_BRANCH_SHAPE = {
+    name: PropTypes.string,
+    path: PropTypes.arrayOf(PropTypes.string),
+    current: PropTypes.bool,
+};
