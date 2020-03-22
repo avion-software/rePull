aa0a0dc62a5ced790cefdfafcaf0492f2fc915ef
Michael Braun <michaelsoftware1997@gmail.com>
2020-03-22 15:39:56 +0100

    Add height to Sidebar
    The sidebar should be of full height

--- src/renderer/components/sidebar/SidebarStyles.js
+++ src/renderer/components/sidebar/SidebarStyles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core';
 const useSidebarStyles = makeStyles((theme) => ({
     root: {
         width: 200,
+        height: '100%',
         backgroundColor: theme.palette.background.sidebar,
         padding: 5,
     },
