8a4d1942ac57b377704bb4e509bef097fb4c9778
Michael Braun <michael@web4brauns.de>
2020-03-29 20:37:00 +0200

    Add BranchItemContext

---
+++ src/renderer/components/sidebar/branch-tree/branch-item/context/BranchItemContext.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BranchItemContext = props => {
+    return (
+        <div>
+            Test
+        </div>
+    );
+};
+
+BranchItemContext.propTypes = {
+
+};
+
+export default BranchItemContext;
--- src/renderer/components/sidebar/branch-tree/branch-item/context/BranchItemContextContainer.jsx
+++ src/renderer/components/sidebar/branch-tree/branch-item/context/BranchItemContextContainer.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import BranchItemContext from './BranchItemContext';
 
 const BranchItemContextContainer = ({ value }) => {
     console.log('value', value);
 
     return (
-        <div>
-            Test
-        </div>
+        <BranchItemContext />
     );
 };
 
