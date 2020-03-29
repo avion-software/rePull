4082f2666a7e01f906f4948286370cf864c6cfea
Michael Braun <michael@web4brauns.de>
2020-03-29 20:56:13 +0200

    Add ContextMenuList component to BranchItemContext

--- src/renderer/components/sidebar/branch-tree/branch-item/context/BranchItemContext.jsx
+++ src/renderer/components/sidebar/branch-tree/branch-item/context/BranchItemContext.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ContextMenuList from '../../../../context-menu/list/ContextMenuList';
+import ContextMenuListItem from '../../../../context-menu/list/ContextMenuListItem';
 
-const BranchItemContext = props => {
-    return (
-        <div>
-            Test
-        </div>
-    );
-};
+const BranchItemContext = () => (
+    <ContextMenuList>
+        <ContextMenuListItem>
+            Test1
+        </ContextMenuListItem>
+        <ContextMenuListItem>
+            Test2
+        </ContextMenuListItem>
+        <ContextMenuListItem>
+            Test3
+        </ContextMenuListItem>
+    </ContextMenuList>
+);
 
 BranchItemContext.propTypes = {
 
