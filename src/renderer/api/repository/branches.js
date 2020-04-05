625c83ca722996e8cb999aaa0f25073c976bfcde
Michael Braun <michael@web4brauns.de>
2020-04-05 22:58:51 +0200

    Add remotes parameter to branches

--- src/renderer/api/repository/branches.js
+++ src/renderer/api/repository/branches.js
@@ -1,7 +1,7 @@
 import request from '../../utils/api/request';
 
-export default async function getRepositoryBranches(repository) {
-    const res = await request(`/repositories/${repository.id}/branches`, 'GET');
+export default async function getRepositoryBranches(repository, remotes = false) {
+    const res = await request(`/repositories/${repository.id}/branches?remotes=${String(!!remotes)}`, 'GET');
 
     if (res.statusCode === 200) {
         return res.json();
