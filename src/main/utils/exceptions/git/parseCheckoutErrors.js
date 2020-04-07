f8d1d166f443cb2fec3910f49a0d6b7e773a04cf
Michael Braun <michael@web4brauns.de>
2020-04-07 22:17:50 +0200

    Add parseCheckoutErrors utility
    This utility should be used to parse checkout errors

---
+++ src/main/utils/exceptions/git/parseCheckoutErrors.js
@@ -0,0 +1,27 @@
+import CheckoutConflictError from './CheckoutConflictError';
+
+export default async function parseCheckoutErrors(ex) {
+    let exceptionType = 0;
+    const paths = [];
+    const lines = String(ex).split('\n');
+
+    for (let i = 0, z = lines.length; i < z; i += 1) {
+        if (lines[i].indexOf('Your local changes to the following files would be overwritten by checkout:') !== -1) {
+            exceptionType = 1;
+            // eslint-disable-next-line no-continue
+            continue;
+        }
+
+        if (exceptionType === 1) {
+            if (lines[i].startsWith('\t')) {
+                paths.push(lines[i].trim());
+            } else {
+                throw new CheckoutConflictError(paths);
+            }
+        }
+    }
+
+    console.error([ex]);
+
+    throw ex;
+}
