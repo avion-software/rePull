9884bc790e694ec93f461df4eaf9a0431ec97028
Michael Braun <michael@web4brauns.de>
2020-04-07 22:23:13 +0200

    Add checkout-endpoint to router

--- src/main/api/repositories/index.js
+++ src/main/api/repositories/index.js
@@ -5,6 +5,7 @@ import pull from './actions/pull';
 import commit from './actions/commit';
 import push from './actions/push';
 import getRepositoryBranches from './get-branches';
+import checkout from './actions/checkout';
 
 export default function createRepositoryEndpoints(server) {
     server.get('/repositories', listRepositories);
@@ -14,4 +15,5 @@ export default function createRepositoryEndpoints(server) {
     server.post('/repositories/:repositoryId/pull', pull);
     server.post('/repositories/:repositoryId/commit', commit);
     server.post('/repositories/:repositoryId/push', push);
+    server.post('/repositories/:repositoryId/checkout', checkout);
 }
