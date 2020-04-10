e57e28e4c9d2b6e50ade5c8d5f1f80e7729547f6
Michael Braun <michaelsoftware1997@gmail.com>
2020-04-11 00:11:00 +0200

    Add shapes for changes

--- src/renderer/constants/shapes.js
+++ src/renderer/constants/shapes.js
@@ -25,3 +25,45 @@ export const REPOSITORY_BRANCH_SHAPE = {
     current: PropTypes.bool.isRequired,
     repositories: PropTypes.shape(REPOSITORY_SHAPE),
 };
+
+export const HUNK_LINE_SHAPE = {
+    type: PropTypes.oneOf(['normal', 'removed', 'added']),
+    line: PropTypes.shape({
+        before: PropTypes.number,
+        after: PropTypes.number,
+    }),
+    content: PropTypes.string,
+};
+
+export const HUNK_SHAPE = {
+    before: PropTypes.shape({
+        from: PropTypes.number,
+        to: PropTypes.number,
+    }),
+    after: PropTypes.shape({
+        from: PropTypes.number,
+        to: PropTypes.number,
+    }),
+    lines: PropTypes.arrayOf(PropTypes.shape(HUNK_LINE_SHAPE)),
+};
+
+export const REPOSITORY_CHANGE_SHAPE = {
+    meta: PropTypes.shape({
+        index: PropTypes.shape({
+            before: PropTypes.string,
+            after: PropTypes.string,
+        }),
+        mode: PropTypes.shape({
+            before: PropTypes.number,
+            after: PropTypes.number,
+        }),
+    }),
+    before: PropTypes.string,
+    after: PropTypes.string,
+    hunks: PropTypes.arrayOf(PropTypes.shape(HUNK_SHAPE)),
+};
+
+export const REPOSITORY_CHANGES_SHAPE = {
+    staged: PropTypes.arrayOf(PropTypes.shape(REPOSITORY_CHANGE_SHAPE)),
+    unstaged: PropTypes.arrayOf(PropTypes.shape(REPOSITORY_CHANGE_SHAPE)),
+};
